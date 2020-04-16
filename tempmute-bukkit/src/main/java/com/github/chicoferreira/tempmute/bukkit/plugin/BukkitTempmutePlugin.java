b61d7765b87ab56940a7c00eb7c599be6164cf2c
chico ferreira <franciscomferreira2311@gmail.com>
2020-04-16 15:12:45 +0100

    Add commons module and fix some module issues.

--- tempmute-bukkit/src/main/java/com/github/chicoferreira/tempmute/bukkit/plugin/BukkitTempmutePlugin.java
+++ tempmute-bukkit/src/main/java/com/github/chicoferreira/tempmute/bukkit/plugin/BukkitTempmutePlugin.java
@@ -2,9 +2,14 @@ package com.github.chicoferreira.tempmute.bukkit.plugin;
 
 import com.github.chicoferreira.tempmute.TempmutePlugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import com.github.chicoferreira.tempmute.commons.TempmutePluginImpl;
 
-public class BukkitTempmutePlugin extends JavaPlugin implements TempmutePlugin {
+public class BukkitTempmutePlugin extends JavaPlugin {
+
+    private TempmutePlugin plugin;
 
     @Override
-    public void onEnable() { }
+    public void onEnable() {
+        plugin = new TempmutePluginImpl();
+    }
 }
---
+++ tempmute-commons/src/main/java/com/github/chicoferreira/tempmute/commons/TempmutePluginImpl.java
@@ -0,0 +1,6 @@
+package com.github.chicoferreira.tempmute.commons;
+
+import com.github.chicoferreira.tempmute.TempmutePlugin;
+
+public class TempmutePluginImpl implements TempmutePlugin {
+}
